344aa36c06a81e48118a9992cc681a54ed5a8c26
Davidnino21 <davidnino_21@yahoo.com>
2023-12-18 15:02:52 -0800

    updated index.js and product-routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -10,13 +10,12 @@ router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
       include: [
-        Category
-        // {
-        //   model: Category
-        // },
-        // {
-        //   model: Tag, through: ProductTag
-        // }
+        {
+          model: Category
+        },
+        {
+          model: Tag, through: ProductTag
+        }
       ]
     })
     res.status(200).json(productData)
